5dd4c9dd3ce6a7bf1ec55bf7899e332ba73baada
Роман Семёнов <romansemenov2109@gmail.com>
2024-10-31 12:14:59 +0200

    Create wan server

--- modules/clients.py
+++ modules/clients.py
@@ -2,7 +2,6 @@ import socket
 
 with socket.socket(family = socket.AF_INET , type = socket.SOCK_STREAM) as client_socket:
     # Підключаємо клієнта  до локального IP та порту
-    client_socket.connect(("192.168.1.29", 8080))
-
+    client_socket.connect(("WAN_IP_SERVER", 8080))
     # Відправляємо дані клієнта та кодує на сервер 
     client_socket.send("hello server!".encode())
--- modules/server.py
+++ modules/server.py
@@ -1,20 +1,27 @@
 import socket
+from threading import Thread
 
-with socket.socket(family = socket.AF_INET , type = socket.SOCK_STREAM) as server_socket:
-    # Для того щоб отримати lan ip потрiбно використати команду (WINDOWS: ipconfig,  LINUX/MACOS: ifconfig en0)
-    server_socket.bind(('192.168.1.29', 8080))
-    # Прослуховування користувачів, очікування підключення 
-    server_socket.listen()
+def start_server():
+    with socket.socket(family = socket.AF_INET , type = socket.SOCK_STREAM) as server_socket:
+        # Для того щоб отримати lan ip потрiбно використати команду (WINDOWS: ipconfig,  LINUX/MACOS: ifconfig en0)
+        server_socket.bind(('0.0.0.0', 8080))
+        # Прослуховування користувачів, очікування підключення 
+        server_socket.listen()
 
-    print("Connecting ...")
+        print("Connecting ...")
 
-    # Приймає підключення користувача
-    # Зберігаємо налаштування клієнту його адресу
-    client_socket, adress = server_socket.accept() # client_socket, adress = (socket, raddr)
+        # Приймає підключення користувача
+        # Зберігаємо налаштування клієнту його адресу
+        client_socket, adress = server_socket.accept() # client_socket, adress = (socket, raddr)
 
-    print("Connected user - ", adress)
+        print("Connected user - ", adress)
 
-    # Отримуємо дані клієнту, розміром 1024 байти і декодуємо їх
-    data = client_socket.recv(1024).decode()
-    
-    print(data)
+        # Отримуємо дані клієнту, розміром 1024 байти і декодуємо їх
+        data = client_socket.recv(1024).decode()
+        
+        print(data)
+
+thread_server = Thread(target= start_server)
+thread_server.start()
+
+print("Create first thread")
